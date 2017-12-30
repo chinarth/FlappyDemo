59deeb8e4055d34f5548f73d544622a835bd3049
chinarth <chinarth@gmail.com>
2017-12-30 00:36:17 -0500

    added horizontal movements, and multiple tubes that loop back once it is out of sight

--- core/src/com/flappydemo/game/Sprites/Tube.java
+++ core/src/com/flappydemo/game/Sprites/Tube.java
@@ -12,7 +12,8 @@ import java.util.Random;
 public class Tube {
     private static final int FLUCTUATION = 130;
     private static final int TUBE_GAP = 100;
-    private static final int LOWEST_OPENING = 120;
+    private static final int LOWEST_OPENING = 80;
+    public static final int TUBE_WIDTH = 52;
 
     private Texture topTube, bottomTube;
     private Vector2 posTopTube, posBotTube;
@@ -41,5 +42,11 @@ public class Tube {
 
     public Vector2 getPosBotTube() {
         return posBotTube;
+    }
+
+    public void reposition(float x){
+        posTopTube.set(x, rand.nextInt(FLUCTUATION)+ TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
     }
 }
--- core/src/com/flappydemo/game/States/PlayState.java
+++ core/src/com/flappydemo/game/States/PlayState.java
@@ -3,6 +3,7 @@ package com.flappydemo.game.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.flappydemo.game.FlappyDemo;
 import com.flappydemo.game.Sprites.Bird;
 import com.flappydemo.game.Sprites.Tube;
@@ -12,17 +13,25 @@ import com.flappydemo.game.Sprites.Tube;
  */
 
 public class PlayState extends State {
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
 
     private Bird bird;
-    private Tube tube;
     private Texture bg;
+
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gam) {
         super(gam);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
-        tube = new Tube(100);
         bg = new Texture("sprites/background-day.png");
+
+        //tube initialization
+        tubes = new Array<Tube>();
+        for (int i = 1; i <= TUBE_COUNT; i++) {
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -36,6 +45,17 @@ public class PlayState extends State {
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        //camera position update, 80 is to offset the bird a little
+        cam.position.x = bird.getPosition().x + 80;
+
+        //tube repositioning logic
+        for (Tube tube : tubes){
+            if (cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+
+        cam.update();
     }
 
     @Override
@@ -44,8 +64,10 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), cam.position.y - (cam.viewportHeight / 2  ));
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
-        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
-        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x, tube.getPosBotTube().y);
+        for (Tube tube: tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         sb.end();
     }
 
