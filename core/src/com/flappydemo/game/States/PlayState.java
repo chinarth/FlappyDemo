3be1bc878455c53ba1c86d88a744adbbf6c317f1
chinarth <chinarth@gmail.com>
2017-12-29 23:57:41 -0500

    added a top pipe sprite, added jump velocity for bird, created random height pipes

--- core/src/com/flappydemo/game/States/PlayState.java
+++ core/src/com/flappydemo/game/States/PlayState.java
@@ -1,9 +1,11 @@
 package com.flappydemo.game.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.flappydemo.game.FlappyDemo;
 import com.flappydemo.game.Sprites.Bird;
+import com.flappydemo.game.Sprites.Tube;
 
 /**
  * Created by Arthur on 2017-12-29.
@@ -12,15 +14,22 @@ import com.flappydemo.game.Sprites.Bird;
 public class PlayState extends State {
 
     private Bird bird;
+    private Tube tube;
+    private Texture bg;
+
     public PlayState(GameStateManager gam) {
         super(gam);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        tube = new Tube(100);
+        bg = new Texture("sprites/background-day.png");
     }
 
     @Override
     protected void handleInput() {
-
+        if (Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
@@ -33,7 +42,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), cam.position.y - (cam.viewportHeight / 2  ));
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
